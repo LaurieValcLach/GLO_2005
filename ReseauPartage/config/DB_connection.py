a629a8271b02a7c76a922bfc2460d78995444d5a
LaurieValcLach <Laurie.Valclach@outlook.com>
2023-03-08 14:32:28 -0500

    erreur syntaxe

--- ReseauPartage/config/DB_connection.py
+++ ReseauPartage/config/DB_connection.py
@@ -4,8 +4,8 @@ Connection a la base de donnee
 import mysql.connector
 
 connection = mysql.connector(
-        host = ""
-        user = ""
+        host = "",
+        user = "",
         password = ""
 )
 cursor = connection.cursor()

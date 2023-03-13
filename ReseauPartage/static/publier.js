3af05931ad54217bd13d87b0461fe071e4d8a502
Marie-Philippe Brassard <marie-philippe.brassard.1@ulaval.ca>
2023-03-13 14:10:52 -0400

    commit

--- ReseauPartage/static/publier.js
+++
@@ -1,9 +0,0 @@
-function onButtonPublier() {
-    var inputElement = document.getElementById("zone-texte")
-    var newPublicationText = inputElement.value
-    var publicationContainer = document.getElementById("publication")
-    var newPublicationElement = document.createElement("div")
-
-    newPublicationElement.innerText = newPublicationText
-    publicationContainer.appendChild(newPublicationElement)
-}

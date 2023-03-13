1a9063739862329032ce081135e7c27835fe2f9d
Marie-Philippe Brassard <marie-philippe.brassard.1@ulaval.ca>
2023-03-13 13:44:55 -0400

    commit

--- ReseauPartage/flask_app.py
+++ ReseauPartage/flask_app.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template
-from Controllers import Comment, Post, Utilisateurs
+from flask import Flask, render_template, request, jsonify
+from ReseauPartage.database.dataPost import insertPost, selectPost
+from ReseauPartage.Controllers import Comment, Post, Utilisateurs
 
 app = Flask(__name__)
 ProfileUtilisateur = {}
@@ -9,19 +10,46 @@ ProfileUtilisateur = {}
 def main():
     return render_template('login.html')
 
+#@app.route("/login/", methods=['POST'])
+#def login_route():
+#    return login.login()
 
-@app.route("/login", methods=['POST'])
-def login_route():
-    return login.login()
+#@app.route("/home/")
+#def home_route():
+#    return home.home()
 
-@app.route("/home")
-def home_route():
-    return home.home()
+#@app.route("/profil/")
+#def profil_route():
+#    return profil.profil()
 
-@app.route("/profil")
-def profil_route():
-    return profil.profil()
+
+@app.route("/publier/")
+def main():
+    posts = selectPost()
+    return render_template('publier.html', posts=posts)
+
+
+@app.route("/addPost/", methods=["POST"])
+def addPost():
+    data = request.json
+    postText = data["text"]
+    insertPost(postText)
+    response = {
+        "status": 200
+    }
+    return jsonify(response)
+
+@app.route("/posts/", methods=["GET"])
+def getPosts():
+    posts = selectPost()
+    response = {
+        "status": 200,
+        "posts": posts
+    }
+
+    return jsonify(response)
+
 
 
 if __name__ == "__main__":
-    app.run(port=8080)
+    app.run()
--- ReseauPartage/static/publier.js
+++ ReseauPartage/static/publier.js
@@ -1,9 +1,40 @@
 function onButtonPublier() {
-    var inputElement = document.getElementById("zone-texte")
-    var newPublicationText = inputElement.value
-    var publicationContainer = document.getElementById("publication")
-    var newPublicationElement = document.createElement("div")
+    var inputElement = document.getElementById("input")
+    var newPostText = inputElement.value
+    var postContainer = document.getElementById("publication")
+    var newPostElement = document.createElement("div")
 
-    newPublicationElement.innerText = newPublicationText
-    publicationContainer.appendChild(newPublicationElement)
+    newPostElement.innerText = newPostText
+    postContainer.appendChild(newPostElement)
+
+    var url = "addPost"
+    fetch(url, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            text: newPostText
+        })
+    })
+
+}
+
+function fetchPosts() {
+    var getUrl = "posts/"
+
+    fetch(getUrl).then(function (response) {
+        return response.json()
+    }).then(function (data) {
+
+        let posts = data.posts
+
+        for (let post of posts) {
+            var postContainer = document.getElementById("publication")
+            var newPostElement = document.createElement("div")
+            newPostElement.innerText = post
+            postContainer.appendChild(newPostElement)
+        }
+
+    })
 }
